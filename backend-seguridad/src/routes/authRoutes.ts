892751ca351e0ae5fa01f68e7f317ed9e2cf9576
Sergio Matamoros <smatamorosn12@gmail.com>
2026-08-17 10:53:09 -0600

    feat: add routes for forgot password, reset password, and change password functionality

--- backend-seguridad/src/routes/authRoutes.ts
+++ backend-seguridad/src/routes/authRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login , forgotPassword, resetPassword, changePassword} from "../controllers/authController.js";
+import { authenticateToken } from "../middlewares/auth.js";
 
 const authRouter = Router();
 
 authRouter.post('/register', register);
-
 authRouter.post('/login',login);
+
+authRouter.post('/forgot-password', forgotPassword);
+authRouter.post('/reset-password', resetPassword);
+
+authRouter.put('/change-passowrd', authenticateToken, changePassword);
 
 export { authRouter}
